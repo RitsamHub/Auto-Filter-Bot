99c74ebffa504e24bc8c2fa4a1e4967ec193b3d3
RitsamHub <ritsam7890@gmail.com>
2026-04-19 21:24:00 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -38,7 +38,7 @@ SILENTX_PREMIUM_UPDATE = """
 <b>🏷️ Genres</b>: {}
 <code>━━━━━━━━━━━━━━━━━━</code>
 
-<b>⚡ Powered By @SilentXBotz</b>
+<b>⚡ Powered By @x_vrubhi</b>
 """
 
 notified_movies = set()
